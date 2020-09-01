d93ac15d2c7cc61ebb9b423b3afad07e5e0251c3
vlzh <vlzh@pm.me>
2020-09-01 20:26:07 +0600

    Update tempaltes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import axios from "axios";
+import $ from "jquery";
 import * as _ from "lodash";
 
 function main() {
+  const jBody = $("body");
   const story_card_tpl = _.template(
-    document.querySelector("#story_card_tpl").innerHTML
+    document.querySelector("#story_card_tpl").innerHTML 
   );
+
   axios.get("http://localhost:9999/api/stories").then((res) => {
     console.log(
       res.data.forEach((story) => {
-        console.log(story_card_tpl({ title: story.title }));
+        jBody.append($(story_card_tpl(story)))
       })
     );
   });
